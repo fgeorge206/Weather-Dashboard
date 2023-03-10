8bacab4b51785b218a53d91454c7c4553a78f53c
Febin George <fgeorge206@gmail.com>
2023-03-10 03:05:44 -0800

    working icon

--- assets/script.js
+++ assets/script.js
@@ -6,9 +6,11 @@ var renderCity = document.querySelector("#city-name")
 var renderTemp = document.querySelector("#temp")
 var renderWind = document.querySelector("#wind")
 var renderHumid = document.querySelector("#humidity")
+var renderIcon = document.querySelector("#icon")
+var forecastData = document.querySelector("#five-day-forecast")
 
 function searchWeatherApi(city){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`).then(function(res){
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`).then(function(res){
         return res.json();
     }).then (function(data){
         renderResults(data)
@@ -16,13 +18,25 @@ function searchWeatherApi(city){
 }
 
 function renderResults(resultsData){
-    console.log("inside of renderResults")
     console.log(resultsData)
-    renderCity.innerHTML = resultsData.name;
-    renderTemp.innerHTML = resultsData.main.temp;
-    renderWind.innerHTML = resultsData.wind.speed;
-    renderHumid.innerHTML = resultsData.main.humidity;
+    for (let i = 0; i < renderResults.length; i++) {
+        const weatherIcon = resultsData.list[0].weather[0].icon;
+        console.log(weatherIcon)
+        renderCity.innerHTML = resultsData.city.name;
+        // renderIcon.innerHTML = resultsData.weather[i].icon;
+        renderTemp.innerHTML = `Temp: ${resultsData.list[i].main.temp}℉`;
+        renderWind.innerHTML = `Wind: ${resultsData.list[i].wind.speed} MPH`;
+        renderHumid.innerHTML = `Humidity: ${resultsData.list[i].main.humidity}%`;
+        renderIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + weatherIcon + '.png')
+    }
 }
+// searchWeatherApi("seattle");
+// searchWeatherApi("portland");
+
+// for (let i = 0; i < renderResults.length; i++) {
+//     const forecastData = renderResults[i];
+//     console.log(forecastData)
+// }
 
 searchForm.addEventListener("submit",function(e){
     e.preventDefault();

12366ddb894a2d6b735bc225d95833bd9bce73d5
Febin George <fgeorge206@gmail.com>
2023-03-10 23:11:06 -0800

    working 5 day forecast

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,7 @@ var renderTemp = document.querySelector("#temp")
 var renderWind = document.querySelector("#wind")
 var renderHumid = document.querySelector("#humidity")
 var renderIcon = document.querySelector("#icon")
+var renderDate = document.querySelector("#date")
 var forecastData = document.querySelector("#five-day-forecast")
 
 function searchWeatherApi(city){
@@ -18,27 +19,69 @@ function searchWeatherApi(city){
 }
 
 function renderResults(resultsData){
-    console.log(resultsData)
-    for (let i = 0; i < renderResults.length; i++) {
-        const weatherIcon = resultsData.list[0].weather[0].icon;
-        console.log(weatherIcon)
-        renderCity.innerHTML = resultsData.city.name;
-        // renderIcon.innerHTML = resultsData.weather[i].icon;
-        renderTemp.innerHTML = `Temp: ${resultsData.list[i].main.temp}℉`;
-        renderWind.innerHTML = `Wind: ${resultsData.list[i].wind.speed} MPH`;
-        renderHumid.innerHTML = `Humidity: ${resultsData.list[i].main.humidity}%`;
-        renderIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + weatherIcon + '.png')
-    }
-}
-// searchWeatherApi("seattle");
-// searchWeatherApi("portland");
+    // console.log(resultsData)
+    const weatherIcon = resultsData.list[0].weather[0].icon;
+    var unixFormat = dayjs.unix(resultsData.list[0].dt).format('MMMM D, YYYY');
 
-// for (let i = 0; i < renderResults.length; i++) {
-//     const forecastData = renderResults[i];
-//     console.log(forecastData)
-// }
+    renderCity.innerHTML = resultsData.city.name;
+    renderTemp.innerHTML = `Temp: ${resultsData.list[0].main.temp}℉`;
+    renderWind.innerHTML = `Wind: ${resultsData.list[0].wind.speed} MPH`;
+    renderHumid.innerHTML = `Humidity: ${resultsData.list[0].main.humidity}%`;
+    renderDate.innerHTML =  unixFormat
+    renderIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + weatherIcon + '.png')
+}
+
+
+
+function renderForcast(city){
+    var forTemp = document.createElement("p");
+    var forDate = document.createElement("p");
+    var forWind = document.createElement("p");
+    var forHumid = document.createElement("p");
+    var forIcon = document.createElement("img");
+    
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`).then(function(res){
+        return res.json();
+    }).then ((data)=>{
+        console.log(data)
+        for (let i = 0; i < data.list.length; i = i+8) {
+            var daysFormat = dayjs.unix(data.list[i].dt).format('MMMM D, YYYY');
+            var fiDayIcon = data.list[i].weather[0].icon;
+            var fiveDayInfo = document.createElement("div");
+            fiveDayInfo.setAttribute("class","card col m-2");
+            fiveDayInfo.innerHTML = 
+            `
+                <p> ${daysFormat} </p>
+                <img src="http://openweathermap.org/img/w/${fiDayIcon}.png" width="50" height "50"/>
+                <p> Temp: ${data.list[i].main.temp}℉ </p>
+                <p> Wind: ${data.list[i].wind.speed} MPH </p>
+                <p> Humidity: ${data.list[i].main.humidity}% </p>
+            `
+            forecastData.append(fiveDayInfo);
+        }
+    })
+
+    // forTemp.innerHTML = `Temp: ${resultsData.list[0].main.temp}℉`;
+    // forWind.innerHTML = `Wind: ${resultsData.list[0].wind.speed} MPH`;
+    // forHumid.innerHTML = ``;
+    // // forDate.innerHTML =  unixFormat
+    // // forIcon.setAttribute('src', 'http://openweathermap.org/img/w/' + weatherIcon + '.png')
+}
+
+
+if(! localStorage.getItem('localCity')){
+    var localCity = []
+    localStorage.setItem('localCity',JSON.stringify(localCity))
+    
+}else{
+    var localCity = JSON.parse(localStorage.getItem('localCity'))
+} for (let i = 0; i < localCity.length; i++) {
+    savedCities(localCity[i])
+}
 
 searchForm.addEventListener("submit",function(e){
     e.preventDefault();
     var termToSearch = searchTerm.value;
-    searchWeatherApi(termToSearch)})
+    searchWeatherApi(termToSearch);
+    renderForcast(termToSearch)
+})
